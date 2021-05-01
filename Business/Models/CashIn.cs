9f7189bc38d8fad4fbcc9f46a20f89b5736863a1
Frank Mensah <menfra@mail.uni-paderborn.de>
2021-05-01 07:51:09 +0200

    modified the cashin and cashout

--- Business/Models/CashIn.cs
+++ Business/Models/CashIn.cs
@@ -7,8 +7,12 @@ namespace Business.Models
     public class CashIn
     {
         public Guid Id { get; set; }
-        public string CashInPersonName { get; set; }
-        public double CashInAmount { get; set; }
+        public string SenderName { get; set; }
+        public string ReceiverName { get; set; }
+        public string ReceiverPhoneNumber { get; set; }
+        public double SendingAmount { get; set; }
+        public bool IsSendingChargePaidBySender { get; set; }
+        public double SendingCost { get; set; }
         public DateTime TransactionDate { get; set; }
         public Guid AttendingEmployeeId { get; set; }
     }
--- Business/Models/CashOut.cs
+++ Business/Models/CashOut.cs
@@ -7,8 +7,9 @@ namespace Business.Models
     public class CashOut
     {
         public Guid Id { get; set; }
-        public string CashInPersonName { get; set; }
-        public double CashOutAmount { get; set; }
+        public string ReceiverName { get; set; }
+        public string ReceiverPhoneNumber { get; set; }
+        public double PaidAmount { get; set; }
         public DateTime TransactionDate { get; set; }
         public Guid AttendingEmployeeId { get; set; }
     }
