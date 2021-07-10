461f4df6d6c839286230920bd34f18e38b5af097
Frank Mensah <menfra@mail.uni-paderborn.de>
2021-07-10 22:17:47 +0200

    Added more validation rules to CashInValidator

--- Business/Validators/CashInValidator.cs
+++ Business/Validators/CashInValidator.cs
@@ -18,9 +18,30 @@ namespace Business
         public CashInValidator()
         {
             RuleFor(c => c.CustomerName).NotEmpty();
-            RuleFor(c => c.BranchMerchantNumber).NotEmpty();
+            RuleFor(c => c.BranchMerchantNumber).NotEmpty().Must(BeLenghtOfTen);
+            RuleFor(c => c.BranchMerchantNumberNetworkName).NotEmpty();
+            RuleFor(c => c.CreateDateTime).GreaterThan(System.DateTime.Now); //Not sure
+            RuleFor(c => c.CreateUserName).NotEmpty();
+            RuleFor(c => c.CustomerPhoneNumber).NotEmpty();
+            RuleFor(c => c.DepositAmount).NotEmpty().GreaterThan(0.0);
+            RuleFor(c => c.DepositorName).NotEmpty();
+            RuleFor(c => c.DepositPhoneNumber).NotEmpty();
+            RuleFor(c => c.DepositPhoneNumberNetworkName).NotEmpty();
+            RuleFor(c => c.History).NotEmpty();
+            RuleFor(c => c.LastProcessName).NotEmpty();
+            RuleFor(c => c.SendingCost).NotEmpty();
+            RuleFor(c => c.TransactionDate).NotEmpty();
+            RuleFor(c => c.TransactionType).NotEmpty(); //Not sure;
 
             // TODO: Add more rules
+        }
+
+        public bool BeLenghtOfTen(string BranchNumber)
+        {
+            if (BranchNumber.Length < 10)
+                return false;
+            else
+                return true;
         }
     }
 }
